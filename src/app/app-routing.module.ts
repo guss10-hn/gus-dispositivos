55c9761f7f73eb8dd18170979bc39d6b49f1ccec
meiuwu2001 <samuel.reyna434@gmail.com>
2024-05-23 18:43:35 -0600

    nuevita

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './view/table/table.component';
+import { HomeComponent } from './view/home/home.component';
 import { FormComponent } from './view/form/form.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch:'full'},
+  {path: 'dashboard', component: HomeComponent},
   {path: 'table', component:TableComponent},
   {path: 'form', component:FormComponent}
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './view/form/form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TableComponent } from './view/table/table.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
+import { HomeComponent } from './view/home/home.component';
+import { FormComponent } from './view/form/form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TableComponent,
     FormComponent,
-    TableComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
